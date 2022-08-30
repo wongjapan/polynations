b03317b669f55f46e417efd61a8e00f7152687fd
WongJapan <kulomilyarder@gmail.com>
2022-08-30 14:33:06 +0700

    delete store

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "store/reducers";
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
